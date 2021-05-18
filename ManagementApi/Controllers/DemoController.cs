b25b9d67e2b91c703146a48068f12facad329281
Jeffry Gonzalez <jeff@hypertheory.com>
2021-05-18 12:02:02 -0400

    Add project files.

---
+++ ManagementApi/Controllers/DemoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementApi.Controllers
+{
+    public class DemoController : ControllerBase
+    {
+
+        // GET /status
+        [HttpGet("/status")]
+        public ActionResult GetStatus()
+        {
+            var response = new StatusResponse
+            {
+                Status = "Looks Good!",
+                LastChecked = DateTime.Now
+            };
+
+            return Ok(response);
+        }
+    }
+
+    public class StatusResponse
+    {
+        public string Status { get; set; }
+        public DateTime LastChecked { get; set; }
+    }
+}
